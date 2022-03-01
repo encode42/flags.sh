424de9e15ccdb0ca90d2c87bfd041d491535652f
Encode42 <me@encode42.dev>
2022-02-28 23:45:44 -0500

    Fix build

--- app/core/components/actionButtons/GitHubLink.tsx
+++ app/core/components/actionButtons/GitHubLink.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from "react";
-import { ActionIcon, Anchor, Group } from "@mantine/core";
+import { ActionIcon, Anchor } from "@mantine/core";
 import { BrandGithub } from "tabler-icons-react";
 import ActionButtonOptions from "./interfaces/ActionButtonOptions";
 
 export function GitHubLink({ filled = false }: ActionButtonOptions): ReactElement {
     return (
-        <ActionIcon color="green" size="lg" component={Anchor} href="https://github.com/Encode42/flags.sh" target="_blank">
+        <ActionIcon color="green" size="lg" component={Anchor}
+            // @ts-ignore
+            href="https://github.com/Encode42/flags.sh"
+
+            // @ts-ignore
+            target="_blank">
             <BrandGithub />
         </ActionIcon>
     );
