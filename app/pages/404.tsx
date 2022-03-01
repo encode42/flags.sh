48a1684267f52cfa0596b1daa5df9f796315418f
Encode42 <me@encode42.dev>
2022-02-28 23:31:04 -0500

    Improve 404 page

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Head, Link, Routes } from "blitz";
+import { Head, Link, Routes, useRouter } from "blitz";
 import { Center, Group, Title, Text, Anchor } from "@mantine/core";
 
 /**
@@ -7,6 +7,9 @@ import { Center, Group, Title, Text, Anchor } from "@mantine/core";
 export default function Page404() {
     const statusCode = 404;
     const title = "This page could not be found";
+
+    const router = useRouter();
+
     return (
         <>
             <Head>
@@ -24,9 +27,16 @@ export default function Page404() {
                         <Title>{statusCode}</Title>
                         <Text>{title}</Text>
                     </Group>
-                    <Link href={Routes.Home()} passHref>
-                        <Anchor>Back Home</Anchor>
-                    </Link>
+                    <Group>
+                        <Anchor onClick={() => {
+                            router.back();
+                        }}>
+                            Back
+                        </Anchor>
+                        <Link href={Routes.Home()} passHref>
+                            <Anchor>Home</Anchor>
+                        </Link>
+                    </Group>
                 </Group>
             </Center>
         </>
