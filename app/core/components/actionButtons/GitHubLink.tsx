afbc3f447898c66c402922565b2e79aac7d91b05
Encode42 <me@encode42.dev>
2022-03-01 07:30:01 -0500

    Open GitHub link in new tab

--- app/core/components/actionButtons/GitHubLink.tsx
+++ app/core/components/actionButtons/GitHubLink.tsx
@@ -1,7 +1,6 @@
 import { siteDetails } from "../../../util/util";
 import { ReactElement } from "react";
-import { Link } from "blitz";
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, Anchor } from "@mantine/core";
 import { BrandGithub } from "tabler-icons-react";
 import ActionButtonOptions from "./interfaces/ActionButtonOptions";
 
@@ -12,10 +11,10 @@ import ActionButtonOptions from "./interfaces/ActionButtonOptions";
  */
 export function GitHubLink({ filled = false }: ActionButtonOptions): ReactElement {
     return (
-        <Link href={siteDetails.links.github}>
+        <Anchor href={siteDetails.links.github} target="_blank">
             <ActionIcon color="green" size="lg" variant={filled ? "filled" : "hover"} title="Visit the GitHub repository">
                 <BrandGithub />
             </ActionIcon>
-        </Link>
+        </Anchor>
     );
 }
