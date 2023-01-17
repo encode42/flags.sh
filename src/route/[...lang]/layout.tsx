bd67cf31cfd5187060c4c03625e82a25a29561f1
Encode42 <me@encode42.dev>
2023-01-17 17:41:09 -0500

    Better client language detection (hopefully)

--- src/route/[...lang]/layout.tsx
+++ src/route/[...lang]/layout.tsx
@@ -7,27 +7,36 @@ import { ColorScheme } from "~/context/color-scheme/wrapper";
 import supportedLocales from "~/generated/supportedLocales.json";
 
 export const onRequest: RequestHandler = ({ url, headers, locale, cookie, params, redirect }) => {
+    // If language is explicitly set
     const langCookie = cookie.get("lang");
 
     let lang: string | undefined = params.lang || langCookie?.value;
     if (!lang) {
+        // Get the client's accepted languages
         const acceptLanguage = headers.get("accept-language");
-
         if (acceptLanguage) {
+            // Get the first accepted language
             lang = acceptLanguage.split(";")[0]?.split(",")[0];
 
+            // Language code might be stored as _ instead of -
             if (!supportedLocales.includes(lang) && lang.includes("-")) {
-                lang = acceptLanguage.split("-")[0];
+                lang = acceptLanguage.replace("-", "_");
+
+                // Get the broad accepted language
+                if (!supportedLocales.includes(lang)) {
+                    lang = acceptLanguage.split("-")[0];
+                }
             }
         }
-
-        if (lang && !supportedLocales.includes(lang)) {
-            lang = undefined;
-        }
     }
-    lang ??= config.defaultLocale.lang;
 
-    if ((langCookie && lang !== config.defaultLocale.lang) && lang !== params.lang) {
+    // Language could not be found
+    if (!lang || !supportedLocales.includes(lang)) {
+        lang = config.defaultLocale.lang;
+    }
+
+    // Redirect to SSG pages
+    if (lang !== config.defaultLocale.lang && lang !== params.lang) {
         throw redirect(302, `/${lang}${url.pathname}`);
     }
 
